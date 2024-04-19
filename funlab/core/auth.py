63152ff454f1b1b1b46c7abb2de0ba204b3a9ba1
SunnyLin <013.lin@gmail.com>
2024-04-19 11:40:56 +0800

    add auth function and updata license

---
+++ funlab/core/auth.py
@@ -0,0 +1,28 @@
+
+from functools import wraps
+from flask_login import current_user
+from flask import render_template
+def role_required(roles:list|tuple):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            if getattr(current_user, 'role', '|@_@|') not in roles:
+                # Redirect to an unauthorized page or show an error message
+                return render_template('error-403.html'), 403
+            # Call the protected route handler function
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+def admin_required():
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            if not getattr(current_user, 'is_admin', False):  # current_user.is_admin:
+                # Redirect to an unauthorized page or show an error message
+                return render_template('error-403.html'), 403
+            # Call the protected route handler function
+            return func(*args, **kwargs)
+        return wrapper
+
+    return decorator
