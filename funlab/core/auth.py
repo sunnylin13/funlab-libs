59c0903499584eb05be8062bb9c16f62b32f152c
sunny <013.lin@gmail.com>
2025-08-31 22:06:30 +0800

    rft: 簡化為不需(), 使用@admin_required

--- funlab/core/auth.py
+++ funlab/core/auth.py
@@ -14,15 +14,12 @@ def role_required(roles:list|tuple):
         return wrapper
     return decorator
 
-def admin_required():
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            if not getattr(current_user, 'is_admin', False):  # current_user.is_admin:
-                # Redirect to an unauthorized page or show an error message
-                return render_template('error-403.html'), 403
-            # Call the protected route handler function
-            return func(*args, **kwargs)
-        return wrapper
-
-    return decorator
+def admin_required(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if not getattr(current_user, 'is_admin', False):
+            # Redirect to an unauthorized page or show an error message
+            return render_template('error-403.html'), 403
+        # Call the protected route handler function
+        return func(*args, **kwargs)
+    return wrapper
